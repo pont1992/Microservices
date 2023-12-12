7a7b1a0dea4ffb38b52ab33d364b94dae8900c55
pont1992 <453234089@qq.com>
2023-12-12 20:25:17 +0100

    1212

---
+++ Service/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        public string Id { get; set; }
+
+
+    }
+}
